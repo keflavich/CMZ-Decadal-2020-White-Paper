7c99dcfdb9eccd594e59af8cc1b3a83cf6c66c60
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-01-18 17:37:47 -0700

    add more supporting files

---
+++ update_bibentries.py
@@ -0,0 +1,39 @@
+import os
+import ads
+import bibtexparser
+
+def update_bibentries():
+
+    parser = bibtexparser.bparser.BibTexParser(common_strings=True)
+    for key, val in list(parser.bib_database.strings.items()):
+        parser.bib_database.strings[val] = val
+        parser.bib_database.strings[val.lower()] = val
+
+    with open('extracted.bib', 'r') as fh:
+        bib_database = bibtexparser.load(fh, parser=parser)
+
+    # this doesn't work, too many oddball cases for the broken parser to handle
+    with open('bibdesk.bib', 'r') as fh:
+        full_bib_database = bibtexparser.load(fh, parser=parser)
+
+    arxivs = []
+
+    for entry in bib_database.entries:
+        if 'doi' in entry:
+            match = [x for x in full_bib_database.entries if 'doi' in x and x['doi'] == entry['doi']][0]
+            #print(entry['journal'], match['journal'], match['eprint'] if 'eprint' in match else "")
+        elif 'journal' in entry and entry['journal'] == 'ArXiv e-prints':
+            match = [x for x in full_bib_database.entries if x['ID'] == entry['ID']][0]
+            print(entry['journal'], match['journal'], match['eprint'] if 'eprint' in match else "")
+            arxivs.append(match['eprint'])
+        else:
+            pass
+            #print(entry['ID'], entry['author'], entry['year'], entry['journal'] if 'journal' in entry else "")
+            #print(list(entry.keys()), entry['journal'] if 'journal' in entry else "", entry['volume'] if 'volume' in entry else '')
+
+    # have to execut adsbibdesk + this
+    print(" ".join(arxivs))
+
+if __name__ == "__main__":
+    assert os.system('bibexport -o extracted.bib saltydisk.aux') == 0
+    update_bibentries()
